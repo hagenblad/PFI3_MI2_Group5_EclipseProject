9ad6107aa44af08c1e67d9c00ddfee588a49818d
felixlof89 <felixlof89@gmail.com>
2015-05-11 14:10:21 +0200

    Började på Ball klassen

---
+++ ScreenSwing/src/se/mah/k3/Ball.java
@@ -0,0 +1,21 @@
+package se.mah.k3;
+
+public class Ball {
+	int speed = 1;
+	int x = 0;
+	int y = 0;
+	int screenWidth = ?;
+	int screenHeight = ?;
+	public void ballSpeed(){
+		x = x + speed;
+	}
+	
+	public void displayBall(){
+		
+	}
+	
+	public void bounceBall(){
+		if x > screenWidth
+		if x < sceenHeight
+	}
+}
