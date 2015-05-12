92a7462356ba8250262f2dc655758956dd5934ce
Manne Westermark <mannewestermark@gmail.com>
2015-05-12 14:38:59 +0200

    Bakgrund fixad

--- ScreenSwing/src/se/mah/k3/DrawPanel.java
+++ ScreenSwing/src/se/mah/k3/DrawPanel.java
@@ -4,10 +4,18 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Random;
 import java.util.Vector;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.firebase.client.ChildEventListener;
@@ -18,15 +26,18 @@ import com.firebase.client.FirebaseError;
 public class DrawPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private Firebase myFirebaseRef;
+	
 	//A vector is like an ArrayList a little bit slower but Thread-safe. This means that it can handle concurrent changes. 
 	private Vector<User> users = new Vector<User>();
 	Font font = new Font("Verdana", Font.BOLD, 20);
 	
 	public DrawPanel() {
+
 		myFirebaseRef = new Firebase("https://pingispong.firebaseio.com/");
 		myFirebaseRef.removeValue(); //Cleans out everything
 		myFirebaseRef.child("ScreenNbr").setValue(Constants.screenNbr);  //Has to be same as on the app. So place specific can't you see the screen you don't know the number
-		 myFirebaseRef.addChildEventListener(new ChildEventListener() {
+		myFirebaseRef.addChildEventListener(new ChildEventListener() {
+			
 			@Override
 			public void onChildRemoved(DataSnapshot arg0) {}
 			
@@ -80,12 +91,21 @@ public class DrawPanel extends JPanel {
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
+		
 		Graphics2D g2= (Graphics2D) g;
 		g2.setFont(font);
 		g2.setColor(Color.LIGHT_GRAY);
 		g2.fillRect(0, 0, getSize().width, getSize().height);
 		g2.setColor(Color.BLACK);
 		g.drawString("ScreenNbr: "+Constants.screenNbr, 10,  20);
+		Image img1 = Toolkit.getDefaultToolkit().getImage("src/images/bakgrund.jpg");
+	    g2.drawImage(img1, 0, 0, 1440, 900, this);
+	    g2.finalize();
+		
+		
+		
+		
+		
 		//Test
 		for (User user : users) {
 			int x = (int)(user.getxRel()*getSize().width);
