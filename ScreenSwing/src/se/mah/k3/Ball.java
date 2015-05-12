c48877fc384c5d0173432034cea965189a7d8fe8
Hans <Hans@skolLaptop>
2015-05-12 15:54:23 +0200

    Conflicts...

--- ScreenSwing/src/se/mah/k3/Ball.java
+++ ScreenSwing/src/se/mah/k3/Ball.java
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-package se.mah.k3;
-
-public class Ball {
-
-	public int x = 100;
-	public int y = 100;
-	int ySpeed = 7;
-	int xSpeed = 10;
-	int screenWidth = 900;
-	int screenHeight = 900;
-	int size = 40;
-	
-	
-	public int getBallXSpeed(){
-		x +=xSpeed;
-		System.out.println("xspeed= "+ x);
-		bounceBall();
-		return x;
-	}
-		
-
-	public int getBallYSpeed(){
-		y +=ySpeed;
-		System.out.println("yspeed= "+ y);
-		bounceBall();
-		return y;
-	}	
-		
-	public void displayBall(){
-		//maybe
-	}
-	
-	public int getSize(){
-		return size;
-	}
-	
-	public void bounceBall(){
-		if (x >= screenWidth - size/2|| x <= 0 + size/2){
-			xSpeed = xSpeed*-1;
-			x += xSpeed;
-		}
-		if (y >= screenHeight - size/2|| y <= 0+  size/2){
-			ySpeed *= -1;
-			y += ySpeed;
-			
-		}
-	}
-	
-	
-}
-=======
 package se.mah.k3;
 import java.util.Random;
 public class Ball {
@@ -127,4 +75,4 @@ public class Ball {
 	
 	
 }
->>>>>>> master
+
--- ScreenSwing/src/se/mah/k3/User.java
+++ ScreenSwing/src/se/mah/k3/User.java
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-package se.mah.k3;
-
-import java.awt.Color;
-
-public class User implements Comparable<User>{
-	
-	private String id;
-	private int xPos;
-	private int yPos;
-	private double xRel=0;
-	private double yRel=0;
-	private int lives;
-	
-	private Color c = new Color(100,100,100);
-	
-	
-	public User(String id, int xPos, int yPos, int lives) {
-		this.id = id;
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.lives = lives;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public int getxPos() {
-		return xPos;
-	}
-	public void setxPos(int xPos) {
-		this.xPos = xPos;
-	}
-	public int getyPos() {
-		return yPos;
-	}
-	public void setyPos(int yPos) {
-		this.yPos = yPos;
-	}
-
-	public double getyRel() {
-		return yRel;
-	}
-	public void setyRel(double yRel) {
-		this.yRel = yRel;
-	}
-	public double getxRel() {
-		return xRel;
-	}
-	public void setxRel(double xRel) {
-		this.xRel = xRel;
-	}
-	
-	public int getLives() {
-		return lives;
-	}
-	public void setLives(int lives) {
-		this.lives = lives;
-	}
-	
-	
-	
-	@Override
-	public int compareTo(User o) {
-		return id.compareTo(o.getId());
-	}
-	public Color getColor() {
-		return c;
-	}
-	public void setColor(Color c) {
-		this.c = c;
-	}
-}
-=======
 package se.mah.k3;
 
 import java.awt.Color;
@@ -156,4 +80,4 @@ public class User implements Comparable<User>{
 		this.c = c;
 	}
 }
->>>>>>> master
+
