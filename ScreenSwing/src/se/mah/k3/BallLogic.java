47929fdebaf812450007ea47c98ca7af0a20f9e7
Hans <Hans@skolLaptop>
2015-05-21 12:50:44 +0200

    Fixed a merge mistake, I hope

--- ScreenSwing/src/se/mah/k3/BallLogic.java
+++ ScreenSwing/src/se/mah/k3/BallLogic.java
@@ -7,6 +7,11 @@ public class BallLogic {
 
 	private Ball bally = new Ball();
 	private User user = new User();
+	
+	public int player1lives = 5;
+	public int player2lives = 5;
+	public int player3lives = 5;
+	public int player4lives = 5;
 
 	Level level = new Level();
 	
@@ -52,16 +57,22 @@ public class BallLogic {
 		if (bally.getXPos()>= level.screenWidth - bally.getSize()/2){
 			 //Random rand = new Random();
 			 bally.setBallXSpeed(-1);
-				//Reset the ball
+			 player2lives--;
+			 System.out.println("player 2 lost a life");
+			 System.out.println(String.valueOf(player2lives));
 			 reMatch();
+			 gameOver();
 		}
 
 		
 			if(bally.getXPos() <= level.relX + bally.getSize()/2){
 				//Random rand2 = new Random();
 				bally.setBallXSpeed(1);
-				//Reset the ball
+				player1lives--;
+				System.out.println("player 1 lost a life");
+				System.out.println(String.valueOf(player1lives));
 				reMatch();
+				gameOver();
 			}
 			
 			//Goal on Y axis
@@ -182,5 +193,30 @@ public class BallLogic {
 		//Insert kill a life
 		
 	}
+	
+	// restarts the match when a player's life count is 0
+		public void gameOver(){
+			if (player1lives == 0){
+				System.out.println("Player 2 wins");
+				reMatch();
+				player1lives = 5;
+				player2lives = 5;
+//				bally.setXPos(screenWidth/2);
+//				bally.setYPos(screenHeight/2);
+//				bally.setBallYSpeed(0);
+//				bally.setBallXSpeed(0);
+			}
+		
+			if(player2lives == 0){
+				System.out.println("Player 1 Wins");
+				reMatch();
+				player1lives = 5;
+				player2lives = 5;
+//				bally.setXPos(screenWidth/2);
+//				bally.setYPos(screenHeight/2);
+//				bally.setBallYSpeed(0);
+//				bally.setBallXSpeed(0);
+			}
+		}
 
 }
