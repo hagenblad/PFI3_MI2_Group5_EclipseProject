26cc71c71ffbd429da2a4d8af17f44019c41cc08
felixlof89 <felixlof89@gmail.com>
2015-05-22 12:59:32 +0200

    kommenterade ut y axeln

--- ScreenSwing/src/se/mah/k3/BallLogic.java
+++ ScreenSwing/src/se/mah/k3/BallLogic.java
@@ -88,20 +88,20 @@ public class BallLogic {
 			
 			
 
-			//Goal on Y axis
-            //Wall 1
-            if (bally.getYPos()<= level.relY + bally.getSize()/2){
-                    bally.setBallYSpeed(1);
-                    reMatch();
-            }
-           
-           
-            //WAll 3
-           
-            if (bally.getYPos()>= level.screenHeight - bally.getSize()/2){
-                    bally.setBallYSpeed(-1);
-                    reMatch();
-            }
+//			//Goal on Y axis
+//            //Wall 1
+//            if (bally.getYPos()<= level.relY + bally.getSize()/2){
+//                    bally.setBallYSpeed(1);
+//                    reMatch();
+//            }
+//           
+//           
+//            //WAll 3
+//           
+//            if (bally.getYPos()>= level.screenHeight - bally.getSize()/2){
+//                    bally.setBallYSpeed(-1);
+//                    reMatch();
+//            }
 			
 		if(bally.getBallYSpeed()== 0){
 			Random r = new Random();
